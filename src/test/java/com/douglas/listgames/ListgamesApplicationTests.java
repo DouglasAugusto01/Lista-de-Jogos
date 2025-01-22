c0aa15ad463480c05d363e4f97f264b04a771e21
Douglas <dodo.augusto1998@gmail.com>
2025-01-22 00:37:00 -0300

    project created

---
+++ src/test/java/com/douglas/listgames/ListgamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglas.listgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListgamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
